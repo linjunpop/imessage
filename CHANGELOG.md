fd7f63eda43b8d80a782a17a587c716145c49ad9
Jun Lin <linjunpop@gmail.com>
2015-05-08 17:24:05 +0800

    Send attachment.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## master
 
+* Now imessage can send attachment with `-a`.
+
 ## [v0.0.2](https://github.com/linjunpop/imessage/tree/v0.0.2)
 
 * Keep ruby 1.9 compatibility [@otzy007](https://github.com/otzy007) (https://github.com/linjunpop/imessage/pull/1)
--- lib/imessage/sender.rb
+++ lib/imessage/sender.rb
@@ -1,9 +1,24 @@
 module Imessage
   class Sender
-    def self.send(options = {message:'', contacts: []})
+    TYPES = [:text, :attachment]
+
+    def deliver(options = {text:nil, attachment:nil, contacts: []})
       options[:contacts].each do |contact|
-        apple_script_file = File.join(File.dirname(File.expand_path(__FILE__)), 'scripts/send_imessage.applescript')
-        `osascript #{apple_script_file} '#{contact}' '#{options[:message]}'`
+        _deliver(options[:text], options[:attachment], contact)
+      end
+    end
+
+    private
+
+    def _deliver(text, attachment, contact)
+      unless text.nil?
+        apple_script_file = File.join(File.dirname(File.expand_path(__FILE__)), 'scripts/send_text.applescript')
+        `osascript #{apple_script_file} '#{contact}' '#{text}'`
+      end
+
+      unless attachment.nil?
+        apple_script_file = File.join(File.dirname(File.expand_path(__FILE__)), 'scripts/send_attachment.applescript')
+        `osascript #{apple_script_file} '#{contact}' '#{attachment}'`
       end
     end
   end
